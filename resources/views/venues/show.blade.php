461bc60bef08b909b98225e42b41b625f77bfea8
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2026-04-11 20:38:36 +0100

    layout adjustments

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -47,7 +47,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
 
             {{-- Score summary card --}}
             <div class="bg-white rounded-lg border border-gray-200 p-6">
