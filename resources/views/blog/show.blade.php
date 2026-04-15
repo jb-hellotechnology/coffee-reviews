e8788d3322dc1e82d97c906b691e4ac9c000685f
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2026-04-15 09:52:02 +0100

    blog 3

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,7 @@
+@php $pageTitle = strip_tags($post['title']['rendered']); @endphp
 <x-app-layout>
-    <x-slot name="title" :value="{!! $post['title']['rendered'] !!}"></x-slot>
+    <x-app-layout>
+        <x-slot name="title">{{ $pageTitle }}</x-slot>
     <x-slot name="description">{{ strip_tags(Str::limit($post['excerpt']['rendered'], 160)) }}</x-slot>
 
     @push('styles')
